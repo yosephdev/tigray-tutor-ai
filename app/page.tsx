be91f41ea08e9d8725ffd37b503b9cd21f7f64cd
info <info@ternafit.org>
2025-10-31 07:55:42 +0000

    Force new deployment after enabling webframeworks

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-'use client';
+''''use client';
 import React from "react";
 import { LandingPage } from "../components/LandingPage";
 
 export default function Home() {
   return (
     <>
-      <h1>This is a test to force a new deployment</h1>
+      <h1>Forcing a new deployment.</h1>
       <LandingPage />
     </>
   );
 }
+'''
